68a917ba8974012ff8fe991f8b375ed0f5e8dabf
jshal001 <43153289+jshal001@users.noreply.github.com>
2025-02-01 12:18:04 -0800

    Add invert tree solution

--- inverttree.py
+++ inverttree.py
@@ -1,27 +1,28 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None): 
+    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-class Solution: 
-    def invertTree(self, root): 
-        """
-        :type root: TreeNode
-        :rtpe: TreeNode
-        """
-        
-        if not root: 
-            return 
-        
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-
-        
+def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    if root == None or (root.left == None or root.right == None): 
         return root
+    invertTree(root.left)
+    invertTree(root.right)
 
-# Time Complexity: O(n)
-# Space Complexity: O(h) s.t. h = height of tree 
+    temp = root.left 
+    root.left = root.right
+    root.right = temp
+
+    return root
+        
+
+
+
+currTree = TreeNode(4, TreeNode(2, TreeNode(1), TreeNode(3)),TreeNode(7, TreeNode(6), TreeNode(9)))
+
+invertTree(currTree)
