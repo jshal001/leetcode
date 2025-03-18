6b3abd6425def0fe9968b33439401fd779c3a52f
jshal001 <43153289+jshal001@users.noreply.github.com>
2025-03-17 18:23:50 -0700

    Pushing solution to Climbing Stairs problem

--- stairs.py
+++ stairs.py
@@ -3,12 +3,20 @@ def climbStairs(n):
     :type n: int
     :rtype: int
     """
-    if n == 1:
-        return 1
-    else:
-        return pow(2, n - 2) + 1
-        
+    if n <= 3: 
+        return n
+    
+    prev1 = 3
+    prev2 = 2
+    curr = 0
 
+    for _ in range(4, n + 1): 
+        curr = prev1 + prev2
+        prev2 = prev1
+        prev1 = curr
 
-print(climbStairs(4))
+    
+    return curr
+
+print(climbStairs(5))
 
